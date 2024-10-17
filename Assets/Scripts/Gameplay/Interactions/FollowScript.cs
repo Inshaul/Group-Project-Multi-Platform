489451e3ec4435469052fbe8ddfc3270b0173de1
OykuPam <ec24170@qmul.ac.uk>
2024-10-17 12:57:00 +0100

    Updated FollowScript
    Added some changes to the cs file,  the enemy starts following the player when entered a certain distance and stops following after a certain distance

--- Assets/Scripts/Gameplay/Interactions/FollowScript.cs
+++ Assets/Scripts/Gameplay/Interactions/FollowScript.cs
@@ -8,6 +8,12 @@ public class FollowScript : MonoBehaviour
 
     public NavMeshAgent enemy;
     public Transform player;
+    public float startFollowingDistance = 10f;
+    public float stopFollowingDistance = 15f;
+
+
+    private bool isFollowing = false; // check is the enemy is following the player
+
 
 
     // Start is called before the first frame update
@@ -19,6 +25,29 @@ public class FollowScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        enemy.SetDestination(player.position);
+
+        float distanceToPlayer = Vector3.Distance(transform.position, player.position);
+
+        if ( distanceToPlayer <= startFollowingDistance && !isFollowing)
+        {
+            isFollowing = true;
+
+        }
+
+
+        else if  (distanceToPlayer > stopFollowingDistance && isFollowing)
+        {
+            isFollowing = false;
+        }
+
+        if (isFollowing)
+        {
+            enemy.SetDestination(player.position);
+        }
+
+        else
+        {
+            enemy.ResetPath();
+        }
     }
 }
