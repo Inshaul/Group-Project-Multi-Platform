fd060ff249f359500720e0951886a86647271ec5
OykuPam <ec24170@qmul.ac.uk>
2024-10-17 19:52:50 +0100

    Fixed an Error

--- Assets/Scripts/Gameplay/Interactions/FollowScript.cs
+++ Assets/Scripts/Gameplay/Interactions/FollowScript.cs
@@ -16,7 +16,10 @@ public class FollowScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (player == null)
+        {
+            player = GameObject.FindWithTag("Player").transform;
+        }
     }
 
     // Update is called once per frame
